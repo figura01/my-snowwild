10fdabc23dfb698a39c20250fcd0bf7b527229c0
Laurent Vuillaume <figura.graphik@gmail.com>
2024-10-07 12:41:08 +0200

    up dev-ci 8

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -4,7 +4,14 @@ const config: Config.InitialOptions = {
   displayName: 'client',
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  transformIgnorePatterns: ['\\.(css|scss|sass)$'],
+  transform: {
+    "^.+\\.(js|ts)$": "ts-jest",
+  },
+  transformIgnorePatterns: [
+      "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$",
+      "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$",
+      "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$",
+  ],
   moduleNameMapper: {
     '\\.(css|scss|sass)$': 'identity-obj-proxy',
   },
